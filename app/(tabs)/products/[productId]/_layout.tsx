4b4b3deb1c57f18099fd510859c6505bbb106229
Eduardo Ramón <edu.rz@hotmail.com>
2024-10-02 09:15:43 -0500

    hide navbar

--- app/(tabs)/products/[productId]/_layout.tsx
+++ app/(tabs)/products/[productId]/_layout.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useUIStore } from "@/store/uiStore";
 
 export default function DoctorLayout() {
+  const {showTabBar, hideTabBar} = useUIStore()
+  
+  // Ocultar tab bar al crearse el componente
+  useEffect(() => {
+    hideTabBar();
+    return () => {
+      showTabBar();
+    }; // Mostrar tab bar al destruirse el componente
+  }, []);
+
   return (
     <>
       <Stack initialRouteName="index">
